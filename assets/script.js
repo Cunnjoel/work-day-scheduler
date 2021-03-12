41dd398a94cabec8b95eefc2efc8531a15cdcfce
Cunnjoel <cunningham.joel84@gmail.com>
2021-03-12 12:49:20 -0600

    update edited javascript

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,34 @@
 var textArea = document.querySelector("textarea")
-
+var saveBtn = $("saveBtn")
+var times = "#9,#10,#11,#12,#13,#14,#15,#16,#17"
+var currentTime = times.split(" ")
 
 function updateTime() {
     var today= moment().format('MMMM Do YYYY, h:mm:ss a');
     $("#currentDay").text(today)
 };
 
+setInterval(updateTime,1000)
 updateTime();
-    setInterval(function(){
-        updateTime();
-},1000);
 
-var currentTime=$('.time-block').each(function(){
+
+
+var colorBlocks=$('.time-block').each(function(){
     const currentElement = $(this);
     console.log(currentElement);
 
-    if (updateTime = currentTime){
-        $("textarea").addClass("present")
-    }else if (updateTime < this){
+    if (moment().isAfter(currentElement)){
         $("textarea").addClass("past")
-    }else $("textarea").addClass("future")
+    }else if (moment().isBefore(currentElement)){
+        $("textarea").addClass("future")
+    }else $("textarea").addClass("present")
     })
     
+console.log(colorBlocks)
+
+saveBtn.on("click", function(){
+    var text = $(this).prev()
+    localStorage.setItem("textArea", textArea);
+});
+console.log(textArea)
+localStorage.getItem(textArea)
