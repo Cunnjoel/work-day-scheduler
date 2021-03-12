02b9e15406c7e23af71d255571d11700d31454b7
Cunnjoel <cunningham.joel84@gmail.com>
2021-03-11 20:08:37 -0600

    updating js with colorchange over time

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,24 @@
+var textArea = document.querySelector("textarea")
+
+
 function updateTime() {
     var today= moment().format('MMMM Do YYYY, h:mm:ss a');
     $("#currentDay").text(today)
 };
 
 updateTime();
-setInterval(function(){
-    updateTime();
+    setInterval(function(){
+        updateTime();
 },1000);
+
+var currentTime=$('.time-block').each(function(){
+    const currentElement = $(this);
+    console.log(currentElement);
+
+    if (updateTime = currentTime){
+        $("textarea").addClass("present")
+    }else if (updateTime < this){
+        $("textarea").addClass("past")
+    }else $("textarea").addClass("future")
+    })
+    
